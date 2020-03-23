036a4b13931454835633bb9922401eb59be090c7
Wally Yang <wallyyang28@gmail.com>
2020-03-23 19:06:29 -0400

    feat: added 4a 4b sql query

---
+++ Checkpoint3/4b.sql
@@ -0,0 +1,7 @@
+SELECT C.Name, C.Email
+  FROM CUSTOMER AS C,
+       (SELECT SUM(P.Actual_Price) AS Total
+          FROM PURCHASE AS P
+         GROUP BY P.Customer) AS R1
+ WHERE R1.Customer = C.Email AND R1.Total > AVG(R1.Customer)
+ GROUP BY P.Customer
