41aed45d39ea31cf7405450f0d3757098c2a5a8f
Wally Yang <wallyyang28@gmail.com>
2020-03-23 22:41:15 -0400

    feat: added sql for 4f

---
+++ Checkpoint3/4f.sql
@@ -0,0 +1,8 @@
+SELECT A.Name, MAX(Total)
+  FROM (SELECT A.AuthorId, P.Actual_Price * COUNT(P.Book) AS Total
+          FROM PURCHASE AS P, BOOK AS B, WRITES AS W
+         WHERE P.Book = B.Isbn AND B.Isbn = W.Isbn
+         GROUP BY W.AuthorId) AS R1,
+       AUTHOR AS A
+ WHERE R1.AuthorId = A.AuthorId
+ GROUP BY A.AuthorId, A.Name
