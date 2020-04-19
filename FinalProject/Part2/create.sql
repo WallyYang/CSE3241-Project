00b548be623d9b4e64ca14bcc3c9189158311bc3
Wally Yang <wallyyang28@gmail.com>
2020-04-19 10:44:55 -0400

    docs: added comments for SQL create

--- FinalProject/Part2/create.sql
+++ FinalProject/Part2/create.sql
@@ -1,3 +1,4 @@
+-- BOOK table
 CREATE TABLE BOOK(
   Isbn CHAR(13) NOT NULL,
   Title VARCHAR(100) NOT NULL,
@@ -7,6 +8,7 @@ CREATE TABLE BOOK(
   PRIMARY KEY (Isbn)
 );
 
+-- INVENTORY table, specify where the books are stored
 CREATE TABLE INVENTORY(
   Isbn CHAR(13) NOT NULL,
   Location VARCHAR(100) NOT NULL,
@@ -15,12 +17,14 @@ CREATE TABLE INVENTORY(
   FOREIGN KEY (Isbn) REFERENCES BOOK(Isbn)
 );
 
+-- AUTHOR table
 CREATE TABLE AUTHOR(
   AuthorID INT NOT NULL,
   Name VARCHAR(100) NOT NULL,
   PRIMARY KEY(AuthorID)
 );
 
+-- PUBLISHER table
 CREATE TABLE PUBLISHER(
   Name VARCHAR(100) NOT NULL,
   Phone CHAR(11),
@@ -28,6 +32,7 @@ CREATE TABLE PUBLISHER(
   PRIMARY KEY(Name)
 );
 
+-- CUSTOMER table
 CREATE TABLE CUSTOMER(
   Email VARCHAR(100) NOT NULL,
   Name VARCHAR(100) NOT NULL,
@@ -36,6 +41,7 @@ CREATE TABLE CUSTOMER(
   PRIMARY KEY(Email)
 );
 
+-- DISCOUNT table, specify the current discounts on books
 CREATE TABLE DISCOUNT(
   Isbn CHAR(13) NOT NULL,
   Buy_limit INT,
@@ -44,6 +50,7 @@ CREATE TABLE DISCOUNT(
   FOREIGN KEY(Isbn) REFERENCES BOOK(Isbn)
 );
 
+-- ACCOUNT table, stores customers with member accounts
 CREATE TABLE ACCOUNT(
   Email VARCHAR(100) NOT NULL,
   Reward_point INT,
@@ -53,12 +60,14 @@ CREATE TABLE ACCOUNT(
   FOREIGN KEY(Email) REFERENCES CUSTOMER(Email)
 );
 
+-- GIFT table, specify gifts for customers
 CREATE TABLE GIFT(
   Email VARCHAR(100) NOT NULL,
   Type VARCHAR,
   FOREIGN KEY(Email) REFERENCES CUSTOMER(Email)
 );
 
+-- WRITES table, specify the writting author of the books
 CREATE TABLE WRITES(
   Isbn CHAR(13) NOT NULL,
   AuthorId VARCHAR(100) NOT NULL,
@@ -67,6 +76,7 @@ CREATE TABLE WRITES(
   FOREIGN KEY(AuthorId) REFERENCES AUTHOR(AuthorId)
 );
 
+-- PUBLISHES table, specify the publisher of each book
 CREATE TABLE PUBLISHES(
   Isbn CHAR(13) NOT NULL,
   Publisher VARCHAR(100) NOT NULL,
@@ -75,6 +85,7 @@ CREATE TABLE PUBLISHES(
   FOREIGN KEY(Publisher) REFERENCES PUBLISHER(Name)
 );
 
+-- PURCHASE table, stores each book sale
 CREATE TABLE PURCHASE(
   Transaction_id INT NOT NULL,
   Customer VARCHAR(100) NOT NULL,
