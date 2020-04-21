8d99d638bb1df7eb4c20a2e90750f18733c038e3
Wally Yang <wallyyang28@gmail.com>
2020-04-20 23:16:47 -0400

    fix: fixed section 2 query k

--- FinalProject/Part1/Section2/query_k.sql
+++ FinalProject/Part1/Section2/query_k.sql
@@ -1,5 +1,5 @@
-SELECT Name, Email
-  FROM CUSTOMER NATURAL JOIN (SELECT SUM Actural_Price AS Total_amount
-                                FROM PURCHASE AS P
-                               GROUP BY P.Customer ) AS TOTAL
-HAVING TOTAL.Toal_amount > AVG (TOTAL.Total_amount)
+SELECT C.Customer, C.Total_amount
+FROM (SELECT P.Customer, SUM(Actual_Price) AS Total_amount
+        FROM PURCHASE AS P
+       GROUP BY P.Customer) AS C
+WHERE C.Total_amount > 50;
