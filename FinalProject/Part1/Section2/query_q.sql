246634e1b4dfbceefff08208042e489102af3fac
Wally Yang <wallyyang28@gmail.com>
2020-04-20 23:41:48 -0400

    fix: fixed section 2 query q

--- FinalProject/Part1/Section2/query_q.sql
+++ FinalProject/Part1/Section2/query_q.sql
@@ -1,7 +1,13 @@
 SELECT A.Name
-  FROM AUTHOR AS A, WRITES AS W, PURCHASE AS P, CUSTOMER AS C,
-       (SELECT C1.Email AS Email, sum(P1.Actual_Price) AS Spent
-          FROM PURCHASE P1, CUSTOMER C1
-         WHERE P1.Customer = C1.Email
-         GROUP BY P1.Customer) AS personal_sum
- WHERE A.AuthorId = W.AuthorId AND W.Isbn = B.Isbn AND C.Email = personal_sum.Email AND personal_sum.Spent > avg(personal_sum.Spent) AND P.Customer = C.Email AND P.Book = B.Isbn
+FROM (SELECT C.Customer, C.Total_amount
+        FROM (SELECT P.Customer, SUM(Actual_Price) AS Total_amount
+          FROM PURCHASE AS P
+               GROUP BY P.Customer) AS C,
+             (SELECT AVG(T.Total_amount) AS Avg_amount
+          FROM (SELECT SUM(Actual_Price) AS Total_amount
+                  FROM PURCHASE AS P
+                 GROUP BY P.Customer) AS T) AS A
+       WHERE C.Total_amount > A.Avg_amount) AS C,
+     AUTHOR AS A, WRITES AS W, PURCHASE AS P
+ WHERE A.AuthorId = W.AuthorId
+   AND P.Book = W.Isbn AND P.Customer = C.Customer
