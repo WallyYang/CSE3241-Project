c4941467dc4fec801812ea2ba47d9df72f1836b2
Wally Yang <wallyyang28@gmail.com>
2020-04-20 23:30:44 -0400

    fix: fixed section 2 query n

--- FinalProject/Part1/Section2/query_n.sql
+++ FinalProject/Part1/Section2/query_n.sql
@@ -1,7 +1,9 @@
 SELECT A.Name, MAX(Total)
-  FROM AUTHOR AS A, (SELECT A.AuthorId, COUNT(P.Book) AS Total
-          			       FROM PURCHASE AS P, BOOK AS B, WRITES AS W
-         		          WHERE P.Book = B.Isbn AND B.Isbn = W.Isbn
-         			        GROUP BY W.AuthorId) AS R1
+  FROM AUTHOR AS A,
+       (SELECT A.AuthorId, COUNT(P.Book) AS Total
+          FROM PURCHASE AS P, BOOK AS B,
+               WRITES AS W, AUTHOR AS A
+         WHERE P.Book = B.Isbn AND B.Isbn = W.Isbn
+         GROUP BY W.AuthorId) AS R1
  WHERE R1.AuthorId = A.AuthorId
  GROUP BY A.AuthorId, A.Name
