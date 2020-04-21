11a43220b67b302be2027da96fe779cb15e26789
Wally Yang <wallyyang28@gmail.com>
2020-04-20 21:38:58 -0400

    fix: fixed section 2 query a

--- FinalProject/Part1/Section2/query_a.sql
+++ FinalProject/Part1/Section2/query_a.sql
@@ -1,3 +1,4 @@
-SELECT Title
-  FROM BOOK AS B
- WHERE B.Price < 10 AND B.Author = 'Pratchett';
+SELECT Title, A.Name
+  FROM BOOK AS B, WRITES AS W, AUTHOR AS A
+ WHERE B.Price < 10
+   AND (B.Isbn = W.Isbn AND W.AuthorId = A.AuthorId);
