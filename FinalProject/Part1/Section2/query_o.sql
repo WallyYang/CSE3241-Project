f865ea1efb3c5bb87e57f23f2452eda23dd468ed
Wally Yang <wallyyang28@gmail.com>
2020-04-20 23:32:33 -0400

    fix: fixed section 2 query o

--- FinalProject/Part1/Section2/query_o.sql
+++ FinalProject/Part1/Section2/query_o.sql
@@ -1,7 +1,7 @@
 SELECT A.Name, MAX(Total)
   FROM AUTHOR AS A, (
     SELECT A.AuthorId, P.Actual_Price * COUNT(P.Book) AS Total
-      FROM PURCHASE AS P, BOOK AS B, WRITES AS W
+      FROM PURCHASE AS P, BOOK AS B, WRITES AS W, AUTHOR AS A
      WHERE P.Book = B.Isbn AND B.Isbn = W.Isbn
      GROUP BY W.AuthorId) AS R1
  WHERE R1.AuthorId = A.AuthorId
