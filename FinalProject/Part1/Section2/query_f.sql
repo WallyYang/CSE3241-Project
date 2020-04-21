21c414b975a87178cbedb3ce6732f93b98081357
Wally Yang <wallyyang28@gmail.com>
2020-04-20 22:50:51 -0400

    fix: fixed section 2 query f

--- FinalProject/Part1/Section2/query_f.sql
+++ FinalProject/Part1/Section2/query_f.sql
@@ -1,6 +1,6 @@
-SELECT L.Customer, L.Num
+SELECT L.Customer, MAX(L.Num)
   FROM PURCHASE AS P, CUSTOMER AS C,
        (SELECT P.Customer AS Customer, COUNT(Book) AS Num
           FROM PURCHASE AS P, CUSTOMER AS C
-         WHERE P.Customer = C.Email) AS L
- WHERE L.Num = MAX(L.Num);
+         WHERE P.Customer = C.Email
+         GROUP BY P.Customer) AS L
