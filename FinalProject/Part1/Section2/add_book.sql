2ff1fbf0bd17354d047ad53ba96346106eda22c3
ChuwenSun <48740769+ChuwenSun@users.noreply.github.com>
2020-04-20 21:10:52 -0400

    Update add_book.sql
    Changed ISBN since it would produce an error: UNIQUE constraint

--- FinalProject/Part1/Section2/add_book.sql
+++ FinalProject/Part1/Section2/add_book.sql
@@ -1,6 +1,6 @@
 INSERT INTO BOOK
-VALUES ('782140661',                        -- ISBN
-        'OCP: Oracle9i Certification Kit',  -- Title
+VALUES ('782140626',                        -- ISBN
+        'OCP: Oracle9i Certification Kit II',  -- Title
         2002,                               -- Year
         104.97,                             -- Price
         'Computer'                          -- Category
