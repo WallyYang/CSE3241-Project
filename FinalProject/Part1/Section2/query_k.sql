9b87c78912f0b83a9038f8819201d202ed40c71e
Wally Yang <wallyyang28@gmail.com>
2020-04-20 23:28:10 -0400

    fix: fixed section 2 query k

--- FinalProject/Part1/Section2/query_k.sql
+++ FinalProject/Part1/Section2/query_k.sql
@@ -1,5 +1,9 @@
 SELECT C.Customer, C.Total_amount
-FROM (SELECT P.Customer, SUM(Actual_Price) AS Total_amount
-        FROM PURCHASE AS P
-       GROUP BY P.Customer) AS C
-WHERE C.Total_amount > 50;
+  FROM (SELECT P.Customer, SUM(Actual_Price) AS Total_amount
+          FROM PURCHASE AS P
+         GROUP BY P.Customer) AS C,
+       (SELECT AVG(T.Total_amount) AS Avg_amount
+          FROM (SELECT SUM(Actual_Price) AS Total_amount
+                  FROM PURCHASE AS P
+                 GROUP BY P.Customer) AS T) AS A
+ WHERE C.Total_amount > A.Avg_amount;
